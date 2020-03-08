cbc45054d4d0172ec28a20e78570bb0bdb1e1d01
Simon Willison <swillison@gmail.com>
2020-03-08 15:13:08 -0700

    Unit test for index page
    Closes #2. Refs #3

---
+++ tests/test_search_all.py
@@ -0,0 +1,37 @@
+import datasette
+from datasette.app import Datasette
+import sqlite_utils
+import pytest
+import httpx
+
+
+@pytest.fixture
+def db_path(tmpdir):
+    path = str(tmpdir / "data.db")
+    db = sqlite_utils.Database(path)
+    db["creatures"].insert_all(
+        [
+            {"name": "Cleo", "description": "A medium sized dog"},
+            {"name": "Siroco", "description": "A troublesome Kakapo"},
+        ]
+    )
+    return path
+
+
+@pytest.mark.asyncio
+async def test_no_form_on_index_if_not_searcable(db_path):
+    app = Datasette([db_path]).app()
+    async with httpx.AsyncClient(app=app) as client:
+        response = await client.get("http://localhost/")
+    assert 200 == response.status_code
+    assert b'<form action="/-/search" method="get">' not in response.content
+
+
+@pytest.mark.asyncio
+async def test_shows_form_if_searchable(db_path):
+    sqlite_utils.Database(db_path)["creatures"].enable_fts(["name", "description"])
+    app = Datasette([db_path]).app()
+    async with httpx.AsyncClient(app=app) as client:
+        response = await client.get("http://localhost/")
+    assert 200 == response.status_code
+    assert b'<form action="/-/search" method="get">' in response.content
