9391251fb86595c5d0020a5b6d60c9dc22f84a6b
Simon Willison <swillison@gmail.com>
2020-11-12 13:52:30 -0800

    Use urls.table() for search results, closes #10

--- datasette_search_all/__init__.py
+++ datasette_search_all/__init__.py
@@ -17,13 +17,22 @@ def menu_links(datasette):
 
 async def search_all(datasette, request):
     searchable_tables = list(await get_searchable_tables(datasette))
+    tables = [
+        {
+            "database": database,
+            "table": table,
+            "url": datasette.urls.table(database, table),
+            "url_json": datasette.urls.table(database, table, format="json"),
+        }
+        for database, table in searchable_tables
+    ]
     return Response.html(
         await datasette.render_template(
             "search_all.html",
             {
                 "q": request.args.get("q") or "",
-                "searchable_tables": searchable_tables,
-                "searchable_tables_json": json.dumps(searchable_tables),
+                "searchable_tables": tables,
+                "searchable_tables_json": json.dumps(tables, indent=4),
             },
         )
     )
