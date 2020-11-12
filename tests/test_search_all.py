a3ae02deef30eb3dac91475ab1ef16c90e83da76
Simon Willison <swillison@gmail.com>
2020-11-11 18:33:41 -0800

    Form now respects base_url setting

--- tests/test_search_all.py
+++ tests/test_search_all.py
@@ -21,7 +21,7 @@ def db_path(tmpdir):
 async def test_no_form_on_index_if_not_searchable(db_path):
     datasette = Datasette([db_path])
     response = await datasette.client.get("/")
-    assert 200 == response.status_code
+    assert response.status_code == 200
     assert '<form action="/-/search" method="get">' not in response.text
 
 
@@ -29,7 +29,7 @@ async def test_no_form_on_index_if_not_searchable(db_path):
 async def test_no_nav_menu_if_not_searchable(db_path):
     datasette = Datasette([db_path])
     response = await datasette.client.get("/")
-    assert 200 == response.status_code
+    assert response.status_code == 200
     assert '<details class="nav-menu">' not in response.text
 
 
@@ -38,7 +38,7 @@ async def test_shows_form_if_searchable(db_path):
     sqlite_utils.Database(db_path)["creatures"].enable_fts(["name", "description"])
     datasette = Datasette([db_path])
     response = await datasette.client.get("/")
-    assert 200 == response.status_code
+    assert response.status_code == 200
     assert '<form action="/-/search" method="get">' in response.text
 
 
@@ -47,7 +47,7 @@ async def test_shows_nav_menu_if_searchable(db_path):
     sqlite_utils.Database(db_path)["creatures"].enable_fts(["name", "description"])
     datasette = Datasette([db_path])
     response = await datasette.client.get("/")
-    assert 200 == response.status_code
+    assert response.status_code == 200
     assert '<details class="nav-menu">' in response.text
 
 
@@ -56,7 +56,7 @@ async def test_search_page(db_path):
     sqlite_utils.Database(db_path)["creatures"].enable_fts(["name", "description"])
     datasette = Datasette([db_path])
     response = await datasette.client.get("/-/search?q=dog")
-    assert 200 == response.status_code
+    assert response.status_code == 200
     content = response.text
     assert '<form action="/-/search" method="get">' in content
     assert "<title>Search: dog</title>" in content
@@ -64,3 +64,14 @@ async def test_search_page(db_path):
         '<li><a href="/data/creatures?_search=dog">Search data: creatures for "dog"</a></li>'
         in content
     )
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("path", ["/", "/-/search"])
+async def test_base_url(db_path, path):
+    sqlite_utils.Database(db_path)["creatures"].enable_fts(["name", "description"])
+    datasette = Datasette([db_path], config={"base_url": "/foo/"})
+    response = await datasette.client.get(path)
+    assert response.status_code == 200
+    assert '<a href="/foo/-/search">' in response.text
+    assert 'action="/foo/-/search"' in response.text
