c980b485ad6d28abaa3bc7d387b268d67d9a5877
Simon Willison <swillison@gmail.com>
2020-11-11 18:19:23 -0800

    Switch tests to use datasette.client

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     packages=["datasette_search_all"],
     entry_points={"datasette": ["search_all = datasette_search_all"]},
     package_data={"datasette_search_all": ["templates/*.html"]},
-    install_requires=["datasette>=0.44"],
-    extras_require={"test": ["pytest", "pytest-asyncio", "httpx", "sqlite-utils"]},
+    install_requires=["datasette>=0.51"],
+    extras_require={"test": ["pytest", "pytest-asyncio", "sqlite-utils"]},
     tests_require=["datasette-search-all[test]"],
 )
--- tests/test_search_all.py
+++ tests/test_search_all.py
@@ -2,7 +2,6 @@ import datasette
 from datasette.app import Datasette
 import sqlite_utils
 import pytest
-import httpx
 
 
 @pytest.fixture
@@ -19,10 +18,9 @@ def db_path(tmpdir):
 
 
 @pytest.mark.asyncio
-async def test_no_form_on_index_if_not_searcable(db_path):
-    app = Datasette([db_path]).app()
-    async with httpx.AsyncClient(app=app) as client:
-        response = await client.get("http://localhost/")
+async def test_no_form_on_index_if_not_searchable(db_path):
+    datasette = Datasette([db_path])
+    response = await datasette.client.get("/")
     assert 200 == response.status_code
     assert b'<form action="/-/search" method="get">' not in response.content
 
@@ -30,9 +28,8 @@ async def test_no_form_on_index_if_not_searcable(db_path):
 @pytest.mark.asyncio
 async def test_shows_form_if_searchable(db_path):
     sqlite_utils.Database(db_path)["creatures"].enable_fts(["name", "description"])
-    app = Datasette([db_path]).app()
-    async with httpx.AsyncClient(app=app) as client:
-        response = await client.get("http://localhost/")
+    datasette = Datasette([db_path])
+    response = await datasette.client.get("/")
     assert 200 == response.status_code
     assert b'<form action="/-/search" method="get">' in response.content
 
@@ -40,9 +37,8 @@ async def test_shows_form_if_searchable(db_path):
 @pytest.mark.asyncio
 async def test_search_page(db_path):
     sqlite_utils.Database(db_path)["creatures"].enable_fts(["name", "description"])
-    app = Datasette([db_path]).app()
-    async with httpx.AsyncClient(app=app) as client:
-        response = await client.get("http://localhost/-/search?q=dog")
+    datasette = Datasette([db_path])
+    response = await datasette.client.get("/-/search?q=dog")
     assert 200 == response.status_code
     content = response.content.decode("utf-8")
     assert '<form action="/-/search" method="get">' in content
