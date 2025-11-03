705269ed643052dfae0fc489d52abb4da3c191ce
Simon Willison <swillison@gmail.com>
2025-11-03 15:07:41 -0800

    datasette>=1.0a20

--- setup.py
+++ setup.py
@@ -29,8 +29,8 @@ setup(
     packages=["datasette_search_all"],
     entry_points={"datasette": ["search_all = datasette_search_all"]},
     package_data={"datasette_search_all": ["templates/*.html"]},
-    install_requires=["datasette>=0.63.1"],
-    python_requires=">=3.7",
+    install_requires=["datasette>=1.0a20"],
+    python_requires=">=3.10",
     extras_require={
         "test": ["pytest", "pytest-asyncio", "sqlite-utils", "nest-asyncio"],
         "playwright": ["pytest-playwright"]
